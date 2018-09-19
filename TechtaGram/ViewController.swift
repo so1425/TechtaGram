d0b081dde768d09d55490beaecf23afd0499f1ea
so1425 <rhc-myao425os@ezweb.ne.jp>
2018-09-19 18:33:32 +0900

    Initial Commit

---
+++ TechtaGram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechtaGram
+//
+//  Created by so1425 on 2018/09/19.
+//  Copyright © 2018年 so1425. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
